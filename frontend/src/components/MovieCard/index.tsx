eeada839406f45ce78b085586e3d89c547c4cda6
jovinbon <jovinbon@hotmail.com>
2022-01-10 22:41:16 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,14 +1,13 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function MovieCard() {
-
   const movie = {
     id: 1,
     image:
       "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
     title: "The Witcher",
-  
   };
 
   return (
@@ -21,7 +20,9 @@ function MovieCard() {
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
       </div>
     </div>
   );
