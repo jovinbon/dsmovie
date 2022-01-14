59a34ab7300056a2c571c210b756cdf074738169
jovinbon <jovinbon@hotmail.com>
2022-01-13 21:18:40 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -0,0 +1,65 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+
+@Service
+public class ScoreService {
+	
+	@Autowired
+	private ScoreRepository scoreRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		User user = userRepository.findByEmail(dto.getEmail());
+		
+		if (user == null) { //se um usuário não existir, salvar um novo usuário
+			user = new User();
+			user.setEmail(dto.getEmail());
+			user = userRepository.saveAndFlush(user);
+		}
+		
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+		
+		Score score = new Score();
+		score.setUser(user);
+		score.setMovie(movie);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		
+		double sum = 0.0;
+		for (Score s : movie.getScores()) { //percorrendo todos os scores e somando
+			sum = sum + s.getValue();
+		}
+		
+		double avg = sum / movie.getScores().size();
+		
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie);
+		
+		MovieDTO movieDto = new MovieDTO(movie);
+		
+		return movieDto;
+	}
+
+}
