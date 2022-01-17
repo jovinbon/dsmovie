25a0d47a091a03b8b24597c32e2f6cf6a20a5110
jovinbon <jovinbon@hotmail.com>
2022-01-16 21:52:38 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,22 +1,17 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 import "./styles.css";
 
-function MovieCard() {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-  };
+type Props = {
+  movie : Movie;
+};
+
+function MovieCard( {movie} : Props ) {
 
   return (
     <div>
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie.image}
-        alt={movie.title}
-      />
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
